4c39cf8b1c43f0fa240ff84a99a62832d7d3f754
Mikhail Dubov <mdubov@gmail.com>
2015-06-29 18:46:38 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2.0'
+
+
+gem 'rails-api'
+
+# Use sqlite3 as the database for Active Record
+gem 'pg'
+gem 'puma'
+gem 'active_model_serializers'
+gem 'rack-cors', require: 'rack/cors'
+
+gem "ember-cli-rails"
+
+group :production do
+  gem 'rails_12factor'
+end
+
+group :development do
+  gem 'byebug'
+  gem 'spring'
+end
+
+group :development, :test do
+  gem 'awesome_print'
+  gem 'rspec-rails'
+  gem 'pry-rails'
+  gem 'factory_girl_rails'
+  gem 'faker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ParotApi::Application.config.secret_token = '60260a4070c3757cde8bee4de09db2e29639795085ee4f1b872ebabf81b89fc3ad19125d3a730db2cbb31da6e0d6d9d20268151a22604bbda03778c1758e9917'
