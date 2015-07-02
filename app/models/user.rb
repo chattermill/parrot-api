bef0f4ce42de127ba0ea54c7228384db23526ffc
Mikhail Dubov <mdubov@gmail.com>
2015-07-02 23:45:59 +0100

    Add mailchimp api service

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ActiveRecord::Base
   has_many :access_tokens, dependent: :destroy
+  has_one :mailchimp_list
 
   validates :email, presence: true, uniqueness: true
 
   def access_token
     access_tokens.active.first
+  end
+
+  def available_mailchimp_lists
+    mailchimp_api.available_lists
+  end
+
+  def mailchimp_api
+    @mailchimp_api ||= MailchimpAPI.new(user: self)
   end
 end
---
+++ app/services/mailchimp_api.rb
@@ -0,0 +1,19 @@
+class MailchimpAPI
+ 
+  def initialize(user:)
+    @client = MailChimp3.new(
+                oauth_access_token: user.mailchimp_auth_token,
+                dc: user.mailchimp_data_center
+              )
+  end
+
+  def available_lists
+    client.lists.get["lists"].map do |list_hash|
+      { mailchimp_id: list_hash["id"], name: list_hash["name"]}
+    end
+  end
+
+  private
+  
+  attr_reader :client
+end
