2238679f2799868139743f39da2220d6dd56ead1
Mikhail Dubov <mdubov@gmail.com>
2015-07-07 10:50:35 +0100

    Clean up

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module ParrotApi
   class Application < Rails::Application
-    # Use the responders controller from the responders gem
-    # config.app_generators.scaffold_controller :responders_controller
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
      config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins 'localhost:4200', '127.0.0.1:4200', 'localhost:3000', '127.0.0.1:3000'
