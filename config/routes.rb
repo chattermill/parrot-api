1a0fbdb5c88c653f4d63b138112f23ba93b48320
Mikhail Dubov <mdubov@gmail.com>
2015-07-10 15:50:33 +0100

    Sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 ParrotApi::Application.routes.draw do
   require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
   
   namespace :api do
     resources :sessions, only: [:create], defaults: {format: :json}
