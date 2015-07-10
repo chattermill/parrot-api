2a16a5cf9c1914917ac06daa835c52fd713a21ac
Mikhail Dubov <mdubov@gmail.com>
2015-07-10 15:49:50 +0100

    Refactoring

--- app/resources/api/campaign_resource.rb
+++ app/resources/api/campaign_resource.rb
@@ -1,5 +1,12 @@
 module Api
   class CampaignResource < JSONAPI::Resource
+    NPS_FIELDS = [
+      :nps_score,
+      :percentage_promoters,
+      :percentage_detractors,
+      :percentage_passives
+    ]
+
     attributes :company_name, 
       :from_name, 
       :background_color, 
@@ -8,18 +15,21 @@ module Api
       :image_url,
       :from_address,
       :company_url,
+      :created_at,
       :nps_score,
       :percentage_promoters,
       :percentage_detractors,
-      :percentage_passives,
-      :created_at
+      :percentage_passives
 
     has_one :user
-
     
     def self.records(options = {})
       context = options[:context]
       context[:current_user].campaigns
+    end
+
+    def self.creatable_fields(context)
+      super - NPS_FIELDS
     end
   end
 end
--- app/services/user_factory.rb
+++ app/services/user_factory.rb
@@ -4,8 +4,7 @@ class UserFactory
   end
 
   def find_or_create_user
-    return find_user if find_user 
-    User.create(user_params)
+    User.create_with(user_params).find_or_create_by(mailchimp_id: mailchimp_user_id)
   end
 
   private
@@ -14,10 +13,6 @@ class UserFactory
 
   def mailchimp_user_id
     user_params[:mailchimp_id]
-  end
-
-  def find_user
-    User.find_by_mailchimp_id(mailchimp_user_id)
   end
 
   def user_params
